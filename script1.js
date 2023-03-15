d841cf63369dfff05beab149d1c378d4e80808d0
Lucas Fagan <lucasfagan@169-231-123-127.wireless.ucsb.edu>
2023-03-14 19:41:41 -0700

    test 5

--- script1.js
+++ script1.js
@@ -216,6 +216,7 @@ function checkVars(){
         }
 
     }
+    //test
 //function to round to n sig figs with no scientific notation (always write out number)
 function round_to_n_sig_figs(x,n) {
     val = Number.parseFloat(x).toPrecision(n);
